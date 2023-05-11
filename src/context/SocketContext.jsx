628101d570ab4d3990562a39ec73690d2cfb50d3
Daniel Mateu <danielmateu86@gmail.com>
2023-05-11 12:22:33 +0200

    instalacion socket.io-client@3.0.1

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -7,8 +7,8 @@ export const SocketContext = createContext();
 export const SocketProvider = ({ children }) => {
 
     
-    const {socket, onLine} = useSocket('https://bandnames-dmp.herokuapp.com/')
-    // const {socket, onLine} = useSocket('http://localhost:8080')
+    // const {socket, onLine} = useSocket('https://bandnames-dmp.herokuapp.com/')
+    const {socket, onLine} = useSocket('http://localhost:8080')
 
     return (
         <SocketContext.Provider value={{
