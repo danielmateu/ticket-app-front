183638f026a4a61c4cf439c1209495cc0e059b12
Daniel Mateu <danielmateu86@gmail.com>
2023-05-11 10:07:28 +0200

    Pantalla de escritorio

--- src/pages/Escritorio.jsx
+++ src/pages/Escritorio.jsx
@@ -1,8 +1,77 @@
+import { CloseCircleOutlined, RightCircleTwoTone } from "@ant-design/icons"
+import { Button, Col, Divider, Row, Typography } from "antd"
 
+const { Title, Text } = Typography
 
 const EscritorioPage = () => {
+
+    const salir = () => {
+        console.log('salir')
+    }
+
+    const siguienteTiquet = () => {
+        console.log('siguienteTiquet')
+    }
+
     return (
-        <div>EscritorioPage</div>
+        <>
+            <Row>
+                <Col span={20}>
+                    <Title level={2}>
+                        Dani
+                    </Title>
+                    <Text>Usted está trabajando en el escritorio:</Text>
+                    <Text type="success">5</Text>
+                </Col>
+
+                <Col span={4} align="right" >
+                    <Button shape="round" onClick={salir} danger style={
+                        {
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <CloseCircleOutlined />
+                        Salir
+                    </Button>
+                </Col>
+            </Row>
+
+            <Divider />
+
+            <Row>
+                <Col>
+                    <Text>Está atendiendo el ticket número:</Text>
+                    <Text
+                        style={{
+                            fontSize: 30,
+                        }}
+                        type="danger"
+                    >
+                        55
+                    </Text>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col offset={18} span={6} align="right">
+                    <Button
+                        shape="round"
+                        onClick={siguienteTiquet}
+                        style={
+                            {
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                    >
+                        {/* <CloseCircleOutlined /> */}
+                        <RightCircleTwoTone />
+                        Siguiente
+                    </Button>
+                </Col>
+            </Row>
+        </>
     )
 }
 
--- src/pages/Ingresar.jsx
+++ src/pages/Ingresar.jsx
@@ -1,68 +1,88 @@
 import { SaveOutlined } from '@ant-design/icons';
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Divider, Form, Input, InputNumber, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 
+const { Title, Text } = Typography
 
 const IngresarPage = () => {
 
+    const navigate = useNavigate()
+
     const onFinish = (values) => {
         console.log('Success:', values);
+        navigate('/escritorio')
     };
+
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 16,
-            }}
-            style={{
-                maxWidth: 600,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Nombre del usuario"
-                name="Usuario"
-                // placeholder="Ingrese su nombre"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Inserte su nombre por favor!',
-                    },
-                ]}
+        <>
+            <Title
+                level={2}
+                style={{
+                    textAlign: 'center',
+                    marginTop: 20,
+                }}
             >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Número de escritorio"
-                name="Escritorio"
-                // placeholder="Ingrese su nombre"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Inserte su número de escritorio!',
-                    },
-                ]}
+                Ingresar
+            </Title>
+            <Divider />
+            <Text>
+                Ingrese su nombre y número de escritorio
+            </Text>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <InputNumber
-                    min={1}
-                    max={40}
-                />
-            </Form.Item>
+                <Form.Item
+                    label="Nombre del usuario"
+                    name="Usuario"
+                    // placeholder="Ingrese su nombre"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Inserte su nombre por favor!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-            {/* <Form.Item
+                <Form.Item
+                    label="Número de escritorio"
+                    name="Escritorio"
+                    // placeholder="Ingrese su nombre"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Inserte su número de escritorio!',
+                        },
+                    ]}
+                >
+                    <InputNumber
+                        min={1}
+                        max={40}
+                    />
+                </Form.Item>
+
+                {/* <Form.Item
                 label="Contraseña"
                 name="password"
                 rules={[
@@ -75,26 +95,27 @@ const IngresarPage = () => {
                 <Input.Password />
             </Form.Item> */}
 
-            <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    shape='round'
-                    style={{
-                        // width: '100%',
-                        backgroundColor: '#4BC0C8',
-                    }} 
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
                 >
-                    <SaveOutlined/>
-                    Ingresar
-                </Button>
-            </Form.Item>
-        </Form>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        shape='round'
+                        style={{
+                            // width: '100%',
+                            backgroundColor: '#4BC0C8',
+                        }}
+                    >
+                        <SaveOutlined />
+                        Ingresar
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     )
 }
 
