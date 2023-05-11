50003f53ac7b75a406c8165bb093f543cbed2914
Daniel Mateu <danielmateu86@gmail.com>
2023-05-11 12:46:34 +0200

    obtener nuevo ticket

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -6,9 +6,9 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    
+
     // const {socket, onLine} = useSocket('https://bandnames-dmp.herokuapp.com/')
-    const {socket, onLine} = useSocket('http://localhost:8080')
+    const { socket, onLine } = useSocket('http://localhost:8080')
 
     return (
         <SocketContext.Provider value={{
--- src/pages/CrearTicket.jsx
+++ src/pages/CrearTicket.jsx
@@ -1,66 +1,55 @@
 import { DownloadOutlined } from "@ant-design/icons"
 import { Button, Col, Divider, Row, Typography } from "antd"
 import { useHideMenu } from "../hooks/useHideMenu"
+import { useContext, useState } from "react"
+import { SocketContext } from "../context/SocketContext"
 
 const { Title, Text } = Typography
 
 const CrearTicketPage = () => {
 
     useHideMenu(true)
+    const { socket } = useContext(SocketContext)
+    const [ultimoTicket, setUltimoTicket] = useState({})
 
     const nuevoTicket = () => {
-        console.log('Nuevo Ticket')
+        // console.log('Nuevo Ticket')
+        socket.emit('solicitar-ticket', null, (ticket) => {
+            console.log(ticket)
+            setUltimoTicket(ticket)
+        })
     }
 
     return (
         <>
             <Row>
-                <Col
-                    span={14} offset={6} align="center"
-                >
-                    <Title
-                        level={3}
-                    >
-                        Presione el botón para un nuevo ticket
-                    </Title>
-
-                    <Button
-                        shape="round"
-                        icon={<DownloadOutlined />}
-                        size="large"
-                        onClick={nuevoTicket}
-                    >
+                <Col span={14} offset={6} align="center">
+                    <Title level={3}>Presione el botón para un nuevo ticket</Title>
+                    <Button shape="round" icon={<DownloadOutlined />} size="large" onClick={nuevoTicket}>
                         Nuevo Ticket
                     </Button>
                 </Col>
             </Row>
 
-            <Row
-                style={{
-                    marginTop: 100,
-                }}
-            >
-                <Col
-                    span={14} offset={6} align="center"
-                >
-                    <Text level={2}>
-                        Su número
-                    </Text>
-                    {/* <Divider/> */}
-                    <Divider>
-                        <Text
-                            type="success"
-                            style={{
-                                fontSize: 55,
-                            }}
-                        >
-
-                            55
-                        </Text>
-                    </Divider>
-                </Col>
-            </Row>
-
+            {
+                ultimoTicket.numero ?
+                    (<Row style={{ marginTop: 100 }}>
+                        <Col span={14} offset={6} align="center">
+                            <Text level={2}>Su número</Text>
+                            {/* <Divider/> */}
+                            <Divider>
+                                <Text type="success" style={{ fontSize: 55, }}>
+                                    {ultimoTicket.numero}
+                                </Text>
+                            </Divider>
+                        </Col>
+                    </Row>) :
+                    (<Row style={{ marginTop: 100, }}>
+                        <Col span={14} offset={6} align="center">
+                            <Text level={2}>No hay tickets en cola</Text>
+                        </Col>
+                    </Row>)
+            }
         </>
     )
 }
